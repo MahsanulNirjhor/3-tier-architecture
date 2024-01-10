0c5996137692c8b1f5a21e37e52ba58f3fc3c8ec
MahsanulNirjhor <MahsanulNirjhor@gmail.com>
2024-01-10 13:57:21 +0600

    3 private subnets has been added for app tier and 3 db sunbets added for rds

--- subnet.tf
+++ subnet.tf
@@ -1,3 +1,4 @@
+# Create 3 Public Subnets
 module "public_subnet_1" {
   source = "./modules/subnet"
   vpc_id = aws_vpc.three-tier-vpc.id
@@ -18,7 +19,6 @@ module "public_subnet_2" {
   
 }
 
-# Create public subnet 3
 module "public_subnet_3" {
   source = "./modules/subnet"
   vpc_id = aws_vpc.three-tier-vpc.id
@@ -27,4 +27,52 @@ module "public_subnet_3" {
   Name = "3-Tier-Public-AZ-3"
   map_public_ip_on_launch = true
   
+}
+
+# Create 3 Private subnets
+module "private_subnet_1" {
+  source = "./modules/subnet"
+  vpc_id = aws_vpc.three-tier-vpc.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+  Name = "3-Tier-Private-AZ-1"
+}
+
+module "private_subnet_2" {
+  source = "./modules/subnet"
+  vpc_id = aws_vpc.three-tier-vpc.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+  Name = "3-Tier-Private-AZ-2"
+}
+
+module "private_subnet_3" {
+  source = "./modules/subnet"
+  vpc_id = aws_vpc.three-tier-vpc.id
+  cidr_block = "10.0.5.0/24"
+  availability_zone = "us-east-1c"
+  Name = "3-Tier-Private-AZ-3"
+}
+
+# Create 3 Subnets for DB
+module "db_subnet_1" {
+  source = "./modules/subnet"
+  vpc_id = aws_vpc.three-tier-vpc.id
+  cidr_block = "10.0.6.0/24"
+  availability_zone = "us-east-1a"
+  Name = "3-Tier-DB-AZ-1"
+}
+module "db_subnet_2" {
+  source = "./modules/subnet"
+  vpc_id = aws_vpc.three-tier-vpc.id
+  cidr_block = "10.0.7.0/24"
+  availability_zone = "us-east-1b"
+  Name = "3-Tier-DB-AZ-2"
+}
+module "db_subnet_3" {
+  source = "./modules/subnet"
+  vpc_id = aws_vpc.three-tier-vpc.id
+  cidr_block = "10.0.8.0/24"
+  availability_zone = "us-east-1c"
+  Name = "3-Tier-DB-AZ-3"
 }
