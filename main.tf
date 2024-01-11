ce6d97351cf6d50602708d9b2ad7a160e8f70b70
MahsanulNirjhor <MahsanulNirjhor@gmail.com>
2024-01-11 08:05:23 +0600

    feat: Add 3 Route Tables and associated them with subnets
    1. Create public route table for web-tier and associate 3
    public subnets with it.
    
    2. Create private route table for application tier and associate
    3 private subnets with it.
    
    3. Create another private subnet for data tier and associate
    db subnets with it.

--- main.tf
+++ main.tf
@@ -20,3 +20,90 @@ resource "aws_internet_gateway" "three-tier-igw" {
     }
   
 } 
+
+# Public Route Table for Web Tier
+resource "aws_route_table" "three-tier-web-rt" {
+    vpc_id = aws_vpc.three-tier-vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.three-tier-igw.id
+    }
+
+    tags = {
+      Name = "3-Tier-Architecture-Web-RT"
+    }
+  
+}
+
+# Attach the 3 public subnets for web tier we created using modules
+resource "aws_route_table_association" "Public_RT_1" {
+  route_table_id = aws_route_table.three-tier-web-rt.id
+  subnet_id = module.public_subnet_1.id
+}
+resource "aws_route_table_association" "Public_RT_2" {
+  route_table_id = aws_route_table.three-tier-web-rt.id
+  subnet_id = module.public_subnet_2.id
+}
+resource "aws_route_table_association" "Public_RT_3" {
+  route_table_id = aws_route_table.three-tier-web-rt.id
+  subnet_id = module.public_subnet_3.id
+}
+
+# Private Route Table for Application Tier
+resource "aws_route_table" "three-tier-app-rt" {
+    vpc_id = aws_vpc.three-tier-vpc.id
+
+    route = {
+      cidr_block = "0.0.0.0/0"
+      nat_gateway_id = aws_nat_gateway.Three_Tier_NAT.id
+    }
+
+    tags = {
+      Name = "3-Tier-Architecture-App-RT"
+    }
+  
+}
+
+# Private Application Route Table Subnet Assoiations
+resource "aws_route_table_association" "Private_RT_1" {
+  route_table_id = aws_route_table.three-tier-app-rt.id
+  subnet_id = module.private_subnet_1.id
+}
+resource "aws_route_table_association" "Private_RT_2" {
+  route_table_id = aws_route_table.three-tier-app-rt.id
+  subnet_id = module.private_subnet_2.id
+}
+resource "aws_route_table_association" "Private_RT_3" {
+  route_table_id = aws_route_table.three-tier-app-rt.id
+  subnet_id = module.private_subnet_3.id
+}
+
+# Private Route Table for Data Tier
+resource "aws_route_table" "three-tier-data-rt" {
+  vpc_id = aws_vpc.three-tier-vpc.id
+
+  route = {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.Three_Tier_NAT.id
+  }
+
+  tags = {
+    Name = "3-Tier-Architecture-Data-RT"
+  }
+  
+}
+
+# Data Route Table Subnet Assoiations
+resource "aws_route_table_association" "Data_RT_1" {
+  route_table_id = aws_route_table.three-tier-data-rt.id
+  subnet_id = module.db_subnet_1.id
+}
+resource "aws_route_table_association" "Data_RT_2" {
+  route_table_id = aws_route_table.three-tier-data-rt.id
+  subnet_id = module.db_subnet_2.id
+}
+resource "aws_route_table_association" "Data_RT_3" {
+  route_table_id = aws_route_table.three-tier-data-rt.id
+  subnet_id = module.db_subnet_3.id
+}
